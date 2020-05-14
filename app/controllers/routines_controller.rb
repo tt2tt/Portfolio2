8f5ea9d697cf2ad720f79d23d02ef90c0933321e
”taro <groveleffect@gmail.com>
2020-05-14 14:57:09 +0900

    習慣投稿機能の作成

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,2 +1,14 @@
 class RoutinesController < ApplicationController
+  def new
+    @routine = Routine.new
+  end
+
+  def create
+    @routine = Routine.new(routine_params)
+    @routine.save
+  end
+
+  def routine_params
+	  params.require(:routine).permit(:name, :content)
+	end
 end
